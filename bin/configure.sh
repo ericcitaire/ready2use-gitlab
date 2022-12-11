41cf8807b2f07897b462d28fd05d8d28a89022db
ericcitaire <eric.citaire@gmail.com>
2022-12-11 17:57:35 +0000

    updates

--- bin/configure.sh
+++ bin/configure.sh
@@ -12,61 +12,77 @@ if [[ -z "${GITLAB_API_TOKEN}" ]] ; then
     exit 1
 fi
 
+http_get() {
+  path="$1"
+
+  curl -fs -X GET \
+    --header "PRIVATE-TOKEN: ${GITLAB_API_TOKEN}" \
+    --header "Content-Type: application/json" \
+    "${GITLAB_URL}${path}"
+}
+
+http_post() {
+  path="$1"
+  data="$2"
+
+  curl -fs -X POST \
+    --header "PRIVATE-TOKEN: ${GITLAB_API_TOKEN}" \
+    --header "Content-Type: application/json" \
+    --data "${data}" \
+    "${GITLAB_URL}${path}"
+}
+
 create_user() {
   user_email="$1"
   user_name="$2"
   user_password="$3"
 
-  curl -s -X POST \
-    --header "PRIVATE-TOKEN: ${GITLAB_API_TOKEN}" \
-    --header "Content-Type: application/json" \
-    --data '{ "email": "'"${user_email}"'", "name": "'"${user_name^}"'", "username": "'"${user_email/@*/}"'", "password": "'"${user_password}"'", "skip_confirmation": true}' \
-    "${GITLAB_URL}/api/v4/users/" 2>&1 >/dev/null
-
-  curl -s -X GET \
-    --header "PRIVATE-TOKEN: ${GITLAB_API_TOKEN}" \
-    "${GITLAB_URL}/api/v4/users" \
-    | jq --arg user_email "${user_email}" --raw-output '.[] | select(.email == $user_email)'
+  http_post \
+      '/api/v4/users/' \
+      '{ "email": "'"${user_email}"'", "name": "'"${user_name^}"'", "username": "'"${user_email/@*/}"'", "password": "'"${user_password}"'", "skip_confirmation": true}' \
+       2>&1 >/dev/null
+  http_get '/api/v4/users' | jq --arg user_email "${user_email}" --raw-output '.[] | select(.email == $user_email)'
 }
 
 greate_group() {
   group_name="$1"
-  curl -s -X POST \
-    --header "PRIVATE-TOKEN: ${GITLAB_API_TOKEN}" \
-    --header "Content-Type: application/json" \
-    --data '{"name": "'"${group_name^}"'", "path": "'"${group_name,,}"'", "visibility": "public", "auto_devops_enabled": false}' \
-    "${GITLAB_URL}/api/v4/groups/" 2>&1 >/dev/null
 
-  curl -s -X GET \
-    --header "PRIVATE-TOKEN: ${GITLAB_API_TOKEN}" \
-    "${GITLAB_URL}/api/v4/groups/${group_name}"
+  http_post \
+      '/api/v4/groups/' \
+      '{"name": "'"${group_name^}"'", "path": "'"${group_name,,}"'", "visibility": "public", "auto_devops_enabled": false}' \
+       2>&1 >/dev/null
+  http_get "/api/v4/groups/${group_name}"
 }
 
 add_group_member() {
   group_id="$1"
   user_id="$2"
-  curl -s -X POST \
-    --header "PRIVATE-TOKEN: ${GITLAB_API_TOKEN}" \
-    --data "user_id=${user_id}&access_level=30" \
-    "${GITLAB_URL}/api/v4/groups/${group_id}/members" 2>&1 >/dev/null
+
+  http_post \
+      "/api/v4/groups/${group_id}/members" \
+      "user_id=${user_id}&access_level=30" \
+      2>&1 >/dev/null
 }
 
 import_project() {
   namespace_id="$1"
   project_name="$2"
   project_url="$3"
-  curl -s -X POST \
-    --header "PRIVATE-TOKEN: ${GITLAB_API_TOKEN}" \
-    --header "Content-Type: application/json" \
-    --data '{ "name": "'"${project_name^}"'", "path": "'"${project_name,,}"'", "namespace_id": "'"${namespace_id}"'", "visibility": "public", "import_url": "'"${project_url}"'", "analytics_access_level": "disabled", "builds_access_level": "disabled", "container_registry_access_level": "disabled", "forking_access_level": "disabled", "issues_access_level": "disabled", "merge_requests_access_level": "disabled", "operations_access_level": "disabled", "pages_access_level": "disabled", "repository_access_level": "enabled", "requirements_access_level": "disabled", "security_and_compliance_access_level": "disabled", "snippets_access_level": "disabled", "wiki_access_level": "disabled"}' \
-    "${GITLAB_URL}/api/v4/projects/" 2>&1 >/dev/null
+
+  http_post \
+      '/api/v4/projects/' \
+      '{ "name": "'"${project_name^}"'", "path": "'"${project_name,,}"'", "namespace_id": "'"${namespace_id}"'", "visibility": "public", "import_url": "'"${project_url}"'", "analytics_access_level": "disabled", "builds_access_level": "disabled", "container_registry_access_level": "disabled", "forking_access_level": "disabled", "issues_access_level": "disabled", "merge_requests_access_level": "disabled", "operations_access_level": "disabled", "pages_access_level": "disabled", "repository_access_level": "enabled", "requirements_access_level": "disabled", "security_and_compliance_access_level": "disabled", "snippets_access_level": "disabled", "wiki_access_level": "disabled"}' \
+      2>&1 >/dev/null
 }
 
+user_names=(student1 student2)
+user_emails=(student1@home.net student2@home.net)
 
-user_names=(student1 student2 student3 student4)
-user_emails=(student1@home.net student2@home.net student3@home.net student4@home.net)
 for (( i=0; i<${#user_names[@]} ; i+=1 )) ; do
-  namespace_id=$(create_user "${user_emails[i]}" "${user_names[i]}" "training" | jq --raw-output '.namespace_id')
+  echo -n "Creating user ${user_names[i]}..."
+  user_info=$(create_user "${user_emails[i]}" "${user_names[i]}" "training")
+  namespace_id=$(echo "${user_info}" | jq --raw-output '.namespace_id')
   import_project "${namespace_id}" "Maven" https://github.com/ericcitaire/example-maven-project.git
   import_project "${namespace_id}" "Node" https://github.com/ericcitaire/example-node-project.git
+  echo " OK"
 done
--- bin/run.sh
+++ bin/run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # CHANGE IT !!
-fqdn=gitlab.example.com
+# fqdn='??'
+fqdn="8000-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}"
 
 # https://gitlab.com/gitlab-org/omnibus-gitlab/-/raw/master/files/gitlab-config-template/gitlab.rb.template
 read -r -d '' GITLAB_OMNIBUS_CONFIG << EOF
-external_url "https://$fqdn"
 gitlab_rails["initial_root_password"] = "training";
 EOF
 
 export GITLAB_OMNIBUS_CONFIG
 
-docker run --rm -it -p 8000:80 -p 2200:22 \
+docker run --rm -it --name gitlab -p 8000:80 -p 8443:443 -p 2200:22 \
   -e GITLAB_OMNIBUS_CONFIG \
   -v $(pwd)/.gitlab/config:/etc/gitlab \
   -v $(pwd)/.gitlab/logs:/var/log/gitlab \
   -v $(pwd)/.gitlab/data:/var/opt/gitlab \
+  --hostname "${fqdn}" \
   gitlab/gitlab-ee:latest
